f258b4040e22f68169be663fb5c6746d2af3b886
ivannissimrch <ivannissimrch@gmail.com>
2024-08-18 15:27:22 -0400

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-  return <h1>Stock DashBoard</h1>;
-}
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
 
-export default App;
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function App() {
+  return (
+    <Box sx={{ flexGrow: 1 }} height={840}>
+      <h1>Stock Dashboard</h1>
+      <Grid container spacing={2} columns={{ xs: 4, md: 4 }} height={790}>
+        <Grid xs={4} md={3} height={790}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid xs={4} md={1}>
+          <Item>xs=8</Item>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
