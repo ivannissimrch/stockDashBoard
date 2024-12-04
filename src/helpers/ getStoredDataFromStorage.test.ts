764c581e42cbc7e29b82f0e6392fe774f6950511
ivannissimrch <ivannissimrch@gmail.com>
2024-12-03 21:31:09 -0500

    Update getSixWeeksStockData test

--- src/helpers/ getStoredDataFromStorage.test.ts
+++ src/helpers/ getStoredDataFromStorage.test.ts
@@ -1,25 +1,13 @@
 import { afterEach, expect, it } from "vitest";
 import getStoredDataFromStorage from "./getStoredDataFromStorage";
-const storedData = {
-  "2024-11-19": {
-    "1. open": "226.9800",
-    "2. high": "230.1600",
-    "3. low": "226.6600",
-    "4. close": "228.2800",
-  },
-  "2024-07-01": {
-    "1. open": "212.0900",
-    "2. high": "217.5100",
-    "3. low": "211.9200",
-    "4. close": "216.7500",
-  },
-};
+import { MOCK_STOCK_DATA } from "./mockStoredStockData";
 
+const storedData = MOCK_STOCK_DATA;
 afterEach(() => {
   localStorage.clear();
 });
 
-it("handles invalid JSON dat", () => {
+it("handles invalid JSON data", () => {
   localStorage.setItem("storedStocksData", "invalid");
   expect(getStoredDataFromStorage()).toBeUndefined();
 });
