ab9ac4fb252482b392885914317369cfafec6407
ivannissimrch <ivannissimrch@gmail.com>
2024-12-08 20:05:53 -0500

    Refactor organizeStocksInGroups

--- src/helpers/getSixWeeksStockData.ts
+++ src/helpers/getSixWeeksStockData.ts
@@ -1,9 +1,18 @@
 import calculateClosingAverages from "./calculateClosingAverages";
+import getStoredDataFromStorage from "./getStoredDataFromStorage";
 import organizeStocksInGroups from "./organizedStocksInGroups";
 import { GroupType } from "./organizedStocksInGroups";
 
 export default function getSixWeeksStockData() {
-  const sixWeeksDailyData = organizeStocksInGroups(GroupType.Week, 6);
+  const stocksData = getStoredDataFromStorage();
+  if (stocksData === undefined || stocksData === null) {
+    return;
+  }
+  const sixWeeksDailyData = organizeStocksInGroups(
+    GroupType.Week,
+    6,
+    stocksData
+  );
   if (sixWeeksDailyData) {
     return calculateClosingAverages(sixWeeksDailyData);
   }
