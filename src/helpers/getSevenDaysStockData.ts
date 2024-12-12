5facde5598c6fc3ca372401829acd68226dd3aa3
ivannissimrch <ivannissimrch@gmail.com>
2024-12-12 12:41:53 -0500

    Add parameter to getSevenDaysStockData

--- src/helpers/getSevenDaysStockData.ts
+++ src/helpers/getSevenDaysStockData.ts
@@ -1,18 +1,16 @@
-import getStoredDataFromStorage from "./getStoredDataFromStorage";
+import { DailyStocksApiResponse } from "./stockApi";
 
-export default function getSevenDaysStockData() {
+export default function getSevenDaysStockData(
+  stocksData: DailyStocksApiResponse
+) {
   const NUMBER_OF_DAYS = 7;
-  const stockDataFromStorage = getStoredDataFromStorage();
-  if (stockDataFromStorage === undefined || stockDataFromStorage === null) {
-    return;
-  }
-  const stockWithDateObjectsKeys = Object.keys(stockDataFromStorage);
+  const stockWithDateObjectsKeys = Object.keys(stocksData);
   const sevenDaysKeys = stockWithDateObjectsKeys.filter(
     (_stock, idx) => idx < NUMBER_OF_DAYS
   );
   const sevenDaysStocks = sevenDaysKeys.map((key) => {
     return {
-      closingPrices: +stockDataFromStorage[key]["4. close"],
+      closingPrices: +stocksData[key]["4. close"],
       date: key,
     };
   });
