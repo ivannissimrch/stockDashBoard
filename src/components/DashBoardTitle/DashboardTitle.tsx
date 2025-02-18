9cbc3e8c24af04cd7a226034f7e60bbbfea8ee9f
ivannissimrch <ivannissimrch@gmail.com>
2025-02-17 22:44:28 -0500

    fix onClick error on dashboard title

--- src/components/DashBoardTitle/DashboardTitle.tsx
+++ src/components/DashBoardTitle/DashboardTitle.tsx
@@ -2,11 +2,17 @@ import "./DashboardTitle.css";
 import { ReactNode } from "react";
 import darkMode from "../../images/dashboardTitle/Dark mode.jpg";
 
-export default function DashboardTitle({ children }: { children: ReactNode }) {
+export default function DashboardTitle({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+}) {
   return (
     <div className="dashboard_title_container">
       <h2 className="dashboard_title">{children}</h2>
-      <img src={darkMode} className="title_icon" />
+      <img src={darkMode} className="title_icon" onClick={onClick} />
     </div>
   );
 }
