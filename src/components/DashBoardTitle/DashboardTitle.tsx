b6648ec8d800e0d61e4c8f2760232fe86f3f9b3b
ivannissimrch <ivannissimrch@gmail.com>
2025-02-17 22:41:26 -0500

    remove onclick from dashboard title

--- src/components/DashBoardTitle/DashboardTitle.tsx
+++ src/components/DashBoardTitle/DashboardTitle.tsx
@@ -2,16 +2,11 @@ import "./DashboardTitle.css";
 import { ReactNode } from "react";
 import darkMode from "../../images/dashboardTitle/Dark mode.jpg";
 
-export default function DashboardTitle({
-  children,
-  onClick,
-}: {
-  children: ReactNode;
-}) {
+export default function DashboardTitle({ children }: { children: ReactNode }) {
   return (
     <div className="dashboard_title_container">
       <h2 className="dashboard_title">{children}</h2>
-      <img src={darkMode} className="title_icon" onClick={onClick} />
+      <img src={darkMode} className="title_icon" />
     </div>
   );
 }
