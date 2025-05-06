d2d1a05367e0396a9a6d002a989cc3be259ceb6e
ivannissimrch <ivannissimrch@gmail.com>
2025-05-06 16:56:21 -0400

    Update types

--- src/helpers/getFiveMonthsStockData.ts
+++ src/helpers/getFiveMonthsStockData.ts
@@ -1,9 +1,11 @@
-import { StocksData } from "../types";
+import { DailyStocksApiResponse } from "../types";
 import calculateClosingAverages from "./calculateClosingAverages";
 import organizeStocksInGroups from "./organizedStocksInGroups";
 import { GroupType } from "./organizedStocksInGroups";
 
-export default function getFiveMonthsStockData(stocksData: StocksData[]) {
+export default function getFiveMonthsStockData(
+  stocksData: DailyStocksApiResponse
+) {
   const fiveMonthsDailyData = organizeStocksInGroups(
     GroupType.Month,
     5,
--- src/helpers/getSixWeeksStockData.ts
+++ src/helpers/getSixWeeksStockData.ts
@@ -1,9 +1,11 @@
-import { StocksData } from "../types";
+import { DailyStocksApiResponse } from "../types";
 import calculateClosingAverages from "./calculateClosingAverages";
 import organizeStocksInGroups from "./organizedStocksInGroups";
 import { GroupType } from "./organizedStocksInGroups";
 
-export default function getSixWeeksStockData(stocksData: StocksData[]) {
+export default function getSixWeeksStockData(
+  stocksData: DailyStocksApiResponse
+) {
   const sixWeeksDailyData = organizeStocksInGroups(
     GroupType.Week,
     6,
